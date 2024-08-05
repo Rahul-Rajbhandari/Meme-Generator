c58539041b7431e074e6e1335f7998ae3b5cb708
Rahul <rahulrajbhandari19@gmail.com>
2024-08-05 23:08:35 +0545

    js structure created

---
+++ main.js
@@ -0,0 +1,31 @@
+function clearAll(){
+
+}
+
+function showMeme(){
+    const randomMemeUrl = getRandomData('meme');
+    console.log('show meme')
+}
+function showJoke(){
+    const randomJokeText = getRandomData('joke');
+}
+function showQuote(){
+    const randomQuoteText = getRandomData('quote');
+}
+function showRiddle(){
+    const randomRiddleText = getRandomData('riddle');
+}
+function revealAnswer(){
+    const randomMemeUrl = getRandomData('meme');
+}
+
+function getRandomData(){
+
+}
+
+const data = {
+    memes,
+    jokes,
+    quotes,
+    riddles
+}
